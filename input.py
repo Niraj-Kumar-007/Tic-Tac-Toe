56f7a26ce9d6551fc0cf9297bf1a30789fff9900
Amit Ranjan <amitranjan297@gmail.com>
2023-01-05 09:54:23 +0000

    Created Game and Ran Successfully

--- input.py
+++ input.py
@@ -1,10 +1,11 @@
-#Input 
+#Input user's sign in the board
 def getUser():
 # num = int(input("Do you want to play with another user? Press 1 \n If not,Want to play with computer? Press 2 \n Want to exit game? Press 0 \n Enter Your Response:"))
   # if num > 2:
   #   raise ValueError("Error! Please input correct response")
   sign = input("Enter the sign you want play(Cross 'X' or Circle 'O'):")
-  
+  if sign.upper() != "X" or sign.upper() != 'O':
+    raise ValueError("Error! Wrong Input (Please Provide 'X' or 'O')")
   return sign.upper()
 # Input board's position in the game --------------------------------    
 def boardinput(board,ch):
